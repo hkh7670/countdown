61818a0e2e740181f3ff591eaae5a881f8dda4a4
Gyuho <hkh7670@gmail.com>
2016-04-05 10:25:36 +0000

    countdown setting end

---
+++ app/controllers/countdown_controller.rb
@@ -0,0 +1,5 @@
+class CountdownController < ApplicationController
+    def main
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'countdown#main'
+  resources :countdown
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
